6ef7dfc0f242d51167dfee478cab5c3c1f6d1ce1
xrosilloprofe <xv.rosillo@edu.gva.es>
2025-04-13 20:13:51 +0200

    20250414

---
+++ src/main/java/es/ieslavereda/proyecto2425_servidor/Proyecto2425ServidorApplication.java
@@ -0,0 +1,13 @@
+package es.ieslavereda.proyecto2425_servidor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proyecto2425ServidorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Proyecto2425ServidorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/es/ieslavereda/proyecto2425_servidor/Proyecto2425ServidorApplicationTests.java
@@ -0,0 +1,13 @@
+package es.ieslavereda.proyecto2425_servidor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proyecto2425ServidorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
